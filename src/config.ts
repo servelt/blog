e86aa2384799eb04c76351fd30a3b17b2fafdb9a
Servelt <serveltrequest@gmail.com>
2026-01-19 21:59:16 +0900

    commit

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Sevelt",
+	title: "Servelt",
 	subtitle: "",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
